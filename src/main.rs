163b6395f372b870ed338ff81a45dde80a71547c
rtthw <172864013+rtthw@users.noreply.github.com>
2024-12-14 10:09:50 -0500

    Setup basic libloading implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+
+
+
+use anyhow::Result;
+
+
+
+fn main() -> Result<()> {
+    test_same_binary()
+}
+
+
+
+fn test_same_binary() -> Result<()> {
+    let current_dir = std::env::current_dir()?;
+    let lib_path = current_dir.join("target/release/liblib.so");
+
+    unsafe {
+        let lib = libloading::Library::new(lib_path)?;
+        println!("FOUND LIB: {:?}", lib);
+    }
+
+    Ok(())
+}
+
+fn test_multiple_binaries() -> Result<()> {
+    todo!()
 }
